5c0f8d2a640ed98019131c77a45141f8aa54df78
Daniel-D3V <personal.danielsilva.d3v@outlook.com>
2023-09-05 06:09:22 -0300

    save

--- src/pages/projects/@root/Molecules/Header/index.tsx
+++ src/pages/projects/@root/Molecules/Header/index.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
     return (
         <header className={styles.Header}>
             <p className={styles.Title} style={SchibstedGrotesk.style}>Trabalho, Projetos e código aberto.</p>
-            <p className={styles.Description}>Veja abaixo alguns de meus projetos criados recentemente, no total já tivem ais de <b>400</b> projetos criados até agora, porem como todos não foram salvos no gtihub é impossível exibir aqui!</p>
+            <p className={styles.Description}>Confira abaixo alguns dos meus projetos mais recentes. Até o momento, já criei mais de <b>400</b> projetos, embora nem todos estejam disponíveis no GitHub ou Portfólio para exibição pública. Alguns desses projetos estão no GitHub, mas, devido a acordos de serviço contratado, não posso disponibilizar o código-fonte.</p>
         </header>
     )
 }
